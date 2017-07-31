a8c9a96fe1c531d2efe09f956d70f75b003f1f41
Jeffery Utter <jeff@jeffutter.com>
2017-07-30 21:32:22 -0500

    Update deps

--- apps/photo_management_api_web/mix.exs
+++ apps/photo_management_api_web/mix.exs
@@ -33,7 +33,7 @@ defmodule PhotoManagementApi.Web.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.0-rc", override: true},
+      {:phoenix, "~> 1.3.0", override: true},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
       {:gettext, "~> 0.11"},
