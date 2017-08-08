c124a7db21e01a2362c41fd0647cc18082dd31aa
Jeffery Utter <jeff@jeffutter.com>
2017-08-08 10:19:00 -0500

    Serve compressed assets

--- preact.config.js
+++ preact.config.js
@@ -1,7 +1,21 @@
+var CompressionPlugin = require("compression-webpack-plugin");
+
 export default (config, env, helpers) => {
   let babel = config.module.loaders.find(({ loader }) =>
     loader.match(/babel-loader/)
   );
   babel.options.plugins.push("transform-flow-strip-types");
   babel.options.plugins.push("graphql-tag");
+
+  if (env.production) {
+    config.plugins.push(
+      new CompressionPlugin({
+        asset: "[path].gz[query]",
+        algorithm: "gzip",
+        test: /\.(js|html)$/,
+        threshold: 10240,
+        minRatio: 0.8
+      })
+    );
+  }
 };
