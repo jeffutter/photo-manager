39d91a32f315c5b0cebadac285d67fc99c498e8c
Jeffery Utter <jeff@jeffutter.com>
2017-08-16 09:05:38 -0500

    Rework header CSS

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,17 +3,47 @@ import { Link } from "preact-router/match";
 import style from "./style";
 
 export default class Header extends Component {
+  state = {
+    open: false
+  };
+
+  toggleNav = () => {
+    this.setState(state => {
+      return Object.assign({}, state, {
+        open: !state.open
+      });
+    });
+  };
+
   render() {
+    const headerClasses = [style.header];
+    if (this.state.open) headerClasses.push(style.openNav);
+    const mobileToggleClasses = [style.mobileToggle];
+    if (this.state.open) mobileToggleClasses.push(style.openNavMobileToggle);
+
     return (
-      <header class={style.header}>
-        <h1>Photo Gallery</h1>
+      <header class={headerClasses.join(" ")}>
+        <a class={style.logo} href="#">
+          Photo Gallery
+        </a>
+        <div class={mobileToggleClasses.join(" ")} onClick={this.toggleNav}>
+          <span />
+          <span />
+          <span />
+        </div>
         <nav>
-          <Link activeClassName={style.active} href="/">
-            Home
-          </Link>
-          <Link activeClassName={style.active} href="/gallery">
-            Gallery
-          </Link>
+          <ul>
+            <li>
+              <Link activeClassName={style.active} href="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link activeClassName={style.active} href="/gallery">
+                Gallery
+              </Link>
+            </li>
+          </ul>
         </nav>
       </header>
     );
